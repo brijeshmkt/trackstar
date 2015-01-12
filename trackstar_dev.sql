c43659d7d156d822f5528d272bc7fa853b212115
Brijesh <brijeshmkt@gmail.com>
2015-01-12 12:42:39 +0530

    all db

--- trackstar_dev.sql
+++ trackstar_dev.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 12, 2015 at 12:09 PM
+-- Generation Time: Jan 12, 2015 at 12:37 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.4.36-1+deb.sury.org~precise+2
 
@@ -19,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `trackstar_dev`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_issue`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `requester_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `create_user_id` int(11) NOT NULL,
+  `update_time` int(11) NOT NULL,
+  `update_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_issue_project` (`project_id`),
+  KEY `FK_issue_owner` (`owner_id`),
+  KEY `FK_issue_requester` (`requester_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -43,6 +68,64 @@ CREATE TABLE IF NOT EXISTS `tbl_project` (
 
 INSERT INTO `tbl_project` (`id`, `name`, `description`, `create_time`, `create_user_id`, `update_time`, `update_user_id`) VALUES
 (1, 'Test project 1', 'This is test project 1 description after validation', '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_project_user_assignment`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_project_user_assignment` (
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `create_user_id` int(11) NOT NULL,
+  `update_time` datetime NOT NULL,
+  `update_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`project_id`,`user_id`),
+  KEY `FK_user_
+project` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `last_login_time` datetime NOT NULL,
+  `create_time` datetime NOT NULL,
+  `create_user_id` int(11) NOT NULL,
+  `update_time` datetime NOT NULL,
+  `update_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_issue`
+--
+ALTER TABLE `tbl_issue`
+  ADD CONSTRAINT `FK_issue_requester` FOREIGN KEY (`requester_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_issue_owner` FOREIGN KEY (`owner_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_issue_project` FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tbl_project_user_assignment`
+--
+ALTER TABLE `tbl_project_user_assignment`
+  ADD CONSTRAINT `FK_user_
+project` ADD FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_project_
+user` ADD FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
